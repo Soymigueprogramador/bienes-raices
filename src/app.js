59a0171e430380d18dc665e7924449c6f00d2791
miguel salazar <yobebe@hotmail.com.ar>
2024-12-07 17:59:54 -0300

    Corregi el problema de las vistas

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 // Importando dependencias
 import express from 'express';
+import path from 'path';
+import { __dirname } from './util.js'; 
 import userRouter from './routes/user.router.js';
 
 // Configurando constantes para el servidor
-export const nameProyect = 'Bienes Raíces';
+const nameProyect = 'Bienes Raíces';
 const app = express();
 const PORT = 8080;
 
 // Middlewares
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Configurando el motor de plantilla, en este caso usamos pug.
+// Configurando el motor de plantillas
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views')); 
 
-// Llamando a las rutas.
-app.use('/auth', userRouter);
+// Configuración de rutas
+app.use('/', userRouter);
 
 // Iniciando el servidor
 app.listen(PORT, () => {
-    console.log(`Bienvenidos a ${nameProyect}, los esperamos en el puerto ${PORT}`);
+  console.log(`Bienvenidos a "${nameProyect}", los esperamos en el puerto ${PORT}`);
 });
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,11 +1,16 @@
+// Importando dependencias
 import express from 'express';
-import { nameProyect } from '../app.js';
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('auth/login');
+// Ruta raíz
+router.get('/', (req, res) => {
+  res.render('auth/index');
 });
 
+// Ruta para iniciar sesión
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
 
 export default router;
