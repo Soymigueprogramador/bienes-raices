ef158747dd8cf336b9c940ae3a2d90c40c6c0e56
miguel salazar <yobebe@hotmail.com.ar>
2024-12-16 20:45:48 -0300

    Se agrego un hasheo para encriptar los password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 // Importaciones necesarias para el modelo de usuario de la base de datos.
-import { DataTypes } from "sequelize";
+/*import { DataTypes } from "sequelize";
 import db from '../config/db.js'; 
+import bcrypt from 'bcrypt';
 
 // Definiendo el modelo de la tabla 'user' en MySQL.
 const User = db.define('user', {
@@ -30,6 +31,12 @@ const User = db.define('user', {
             notEmpty: { msg: "El campo contraseña no puede estar vacío" }
         }
     },
+    hooks: {
+        beforeCreate: async function(User) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt); 
+        },
+    },
     token: {
         type: DataTypes.STRING,
         allowNull: true 
@@ -42,5 +49,45 @@ const User = db.define('user', {
 }, {
     timestamps: true, 
 });
+
+export default User;*/
+
+
+
+
+import { DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+import sequelize from '../config/db.js';
+
+const User = sequelize.define(
+    'User',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, 
+            validate: {
+                isEmail: true, 
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        hooks: {
+            // Hook para encriptar la contraseña antes de crear el usuario.
+            beforeCreate: async (user) => {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            },
+        },
+    }
+);
 
 export default User;
